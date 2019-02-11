2cd15c8c166494e23ed6eb281538e21131a1fed3
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-02-11 09:33:58 +0100

    Add project files.

---
+++ ConsoleInheretence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleInheretence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
